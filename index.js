81d6b8f40334c34e5e71d2b2c43bd24d6454e5e1
Gabriel Macario <gmacarioweb@gmail.com>
2024-09-09 17:38:25 -0700

    Scaffold socket.io migration

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-const { WebSocket, WebSocketServer } = require("ws");
-const http = require("http");
-const uuidv4 = require("uuid").v4;
+const { Server } = require("socket.io");
 
-const wss = new WebSocketServer({ port: 8080 });
+const io = new Server(3000, { 
+    cors: {
+        origin: "http://localhost:5173"
+    }
+});
 
-const clients = {};
-
-wss.on("connection", function (ws) {
-    const userId = uuidv4();
-    console.log(`${userId } connected!`);
-
-    clients[userId] = ws;
-    ws.on("error", console.error);
-
-    ws.on("message", function message(data) {
-        console.log("received: %s", data);
-        wss.clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(data.toString());
-            }
-        })
-    })
-
-    ws.on('close', () => {
-        console.log(`${userId} disconnected!`);
-    })
+io.on("connection", (socket) => {
+    console.log("CONNECTION RECEIVED")
 })
