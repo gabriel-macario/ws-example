80124d29bbcdda6fb57daf8fdc034679c0ba74f9
Gabriel Macario <gmacarioweb@gmail.com>
2024-09-10 21:13:26 -0700

    Remove unused code

--- index.js
+++ index.js
@@ -20,25 +20,3 @@ const onConnection = ( socket ) => {
 }
 
 io.on("connection", onConnection);
-/*
-wss.on("connection", function (ws) {
-    const userId = uuidv4();
-    console.log(`${userId } connected!`);
-
-    clients[userId] = ws;
-    ws.on("error", console.error);
-
-    ws.on("message", function message(data) {
-        console.log("received: %s", data);
-        wss.clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(data.toString());
-            }
-        })
-    })
-
-    ws.on('close', () => {
-        console.log(`${userId} disconnected!`);
-    })
-})
-*/
